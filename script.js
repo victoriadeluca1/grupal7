baa6660dc2240b2262baa9a02534bcb4b93a522e
clauxdgs <clau.guesan@gmail.com>
2022-11-03 12:16:07 -0300

    add and search functions

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let array = [];
 const URL = "https://6363a39537f2167d6f7eb626.mockapi.io/users";
+let jsonData = [];
 let inputBusqueda = document.getElementById("inputGet1Id");
 let btnBusqueda = document.getElementById("btnGet1");
 
@@ -7,60 +8,32 @@ let inputNameAgregar = document.getElementById("inputPostNombre");
 let inputLastNameAgregar = document.getElementById("inputPostApellido");
 let btnAgregar = document.getElementById("btnPost");
 
-  let getJSONData = function(url){
-      let result = {};
-    //   showSpinner();
-      return fetch(url)
-      .then(response => {
-        if (response.ok) {
-          return response.json();
-        }else{
-          throw Error(response.statusText);
-        }
-      })
-      .then(function(response) {
-            result.status = 'ok';
-            result.data = response;
-            // hideSpinner();
-            return result;
-      })
-      .catch(function(error) {
-          result.status = 'error';
-          result.data = error;
-        //   hideSpinner();
-          return result;
-      });
-  };
+async function updateList() {
+  const jsonData = await fetch(URL).then((response) => response.json());
+  showUsers(jsonData);
+}
 
-const jsonData = await fetch(URL).then((response) => response.json()); //requiere => type="module"
+async function buscar() {
+  const idbusqueda = inputBusqueda.value;
 
-console.log(jsonData)
-
-
-function busqueda() {
-    let valor = inputBusqueda.value;
-    let result = [];
-
-    if ( valor == "" ) {
-        showUsers(jsonData);
-    }
-    else {result = jsonData.filter(objeto => {
-        return objeto.id.includes(valor);
-    });
-    showUsers(result)
-    valor = "";
- }
-};
+  if (idbusqueda == "") {
+    let jsonData = await fetch(URL).then((response) => response.json());
+    showUsers(jsonData);
+  } else {
+    let jsonData = await fetch(URL + `/${idbusqueda}`).then((response) => response.json());
+    showUsers([jsonData])
+  }
+}
 
 //Muestra las categorias en el html
-function showUsers(array){
+function showUsers(array) {
 
-    let htmlContentToAppend = "";
-    for(let i = 0; i < array.length; i++){
-        let elemento = array[i];
+  let htmlContentToAppend = "";
+  for (let i = 0; i < array.length; i++) {
+    let elemento = array[i];
 
-            htmlContentToAppend += `
-            <div onclick="setCatID(${elemento.id})" class="list-group-item list-group-item-action cursor-active">
+    htmlContentToAppend += `
+            <div class="list-group-item list-group-item-action cursor-active">
                 <div class="row">
                     <div class="col">
                         <div class="d-flex w-100 justify-content-between">
@@ -72,49 +45,44 @@ function showUsers(array){
                 </div>
             </div>
             `
-        }
+  }
 
-        document.getElementById("results").innerHTML = htmlContentToAppend;
-    }
-
-    
-
-
-
-
-//DISABLED ENABLED
-
-if (inputNameAgregar.value !== "" && inputLastNameAgregar.value !== "" ) {
-    btnAgregar.removeAttribute("disabled", "");
-    btnAgregar.setAttribute("enabled", "");
+  document.getElementById("results").innerHTML = htmlContentToAppend;
 }
 
-
+function habilitarBTN() {
+  if (inputLastNameAgregar.value === "" && inputLastNameAgregar.value === "") {
+    btnAgregar.removeAttribute("disabled", true);
+  } else {
+    btnAgregar.removeAttribute("disabled", false);
+  }
+}
 
 //POST
+btnAgregar.addEventListener("click", async function () {
+  const data = {
+    name: inputNameAgregar.value,
+    lastname: inputLastNameAgregar.value
+  };
 
-
-btnAgregar.addEventListener("click" , function() {
- let data = { id: (jsonData.length + 1) ,name: inputNameAgregar.value, lastname: inputLastNameAgregar.value};
-
-fetch(URL, {
-  method: 'POST', // or 'PUT'
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify(data),
-})
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(response.json(data))
-    console.log('Success:', data);
-  })
-  .catch((error) => {
-    console.error('Error:', error);
+  const res = await fetch(URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
   });
- busqueda();
+
+  if (res.ok === false) {
+    console.error('Hubo un error al hacer la petición');
+    return;
+  }
+
+  await updateList(); //actualiza la lista con la informacion del servidor
+  inputNameAgregar.value = "";
+  inputLastNameAgregar.value = "";
 });
 
 
 
-
+//INPUTS
