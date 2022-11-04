f696b4a8498f46154369f1eb5a8ca24fcd2524fa
clauxdgs <clau.guesan@gmail.com>
2022-11-04 12:40:44 -0300

    search updated

--- script.js
+++ script.js
@@ -7,20 +7,21 @@ let btnBusqueda = document.getElementById("btnGet1");
 let inputNameAgregar = document.getElementById("inputPostNombre");
 let inputLastNameAgregar = document.getElementById("inputPostApellido");
 let btnAgregar = document.getElementById("btnPost");
-let btnmodificar=document.getElementById("btnPut");
+let btnmodificar = document.getElementById("btnPut");
 let inputModificar = document.getElementById("inputPutId");
-let btnEliminar=document.getElementById("btnDelete");
+let btnEliminar = document.getElementById("btnDelete");
 let inputEliminar = document.getElementById("inputDelete");
 let inputNamemodal = document.getElementById("inputPutNombre");
 let inputLastNamemodal = document.getElementById("inputPutApellido");
 let modal = document.getElementById("dataModal");
+let alerta = document.getElementById("alerta");
 
-btnmodificar.addEventListener("click", async function() {
+btnmodificar.addEventListener("click", async function () {
   let modificarValue = inputModificar.value;
   localStorage.setItem("dataModificar", modificarValue);
 
   const jsonData = await fetch(URL + `/${modificarValue}`).then((response) => response.json());
-console.log(jsonData);
+  console.log(jsonData);
 
   inputNamemodal.value = jsonData.name;
   console.log(jsonData.name)
@@ -37,36 +38,36 @@ async function updateList() {
 //GET
 async function buscar() {
   const idbusqueda = inputBusqueda.value;
+  let id;
+  let ids = [];
+  let jsonData = await fetch(URL).then((response) => response.json());
+
 
   if (idbusqueda == "") {
-    let jsonData = await fetch(URL).then((response) => response.json());
-    showUsers(jsonData);
-
+    updateList();
   }
-  else {
-    let jsonData = await fetch(URL + `/${idbusqueda}`).then((response) => response.json());
-  
+  jsonData.forEach(element => {
+    id = element.id;
+    ids.push(id);
+  });
+  console.log(ids);
+  if (ids.includes(idbusqueda)) {
+    console.log(idbusqueda);
+    let jsonData2 = await fetch(URL + `/${idbusqueda}`).then((response) => response.json());
+    showUsers([jsonData2]);
+  } else {
+    alerta.classList.remove("hide");
+    alerta.classList.add("show");
 
-    if (jsonData.ok === false) {
-      let alerta = document.getElementById("alerta")
-      alerta.classList.remove("hide");
-      alerta.classList.add("show");
+    setTimeout(function () {
 
-      setTimeout(function () {
-        
-        alerta.classList.remove("show");
-        alerta.classList.add("hide");
-      }, "3000")
-          
-
-          // updateList()
-    }
-    else {
-      showUsers([jsonData])
-    }
+      alerta.classList.remove("show");
+      alerta.classList.add("hide");
+    }, "3000")
+    //console.log("noexistewey")
   }
 
-   inputBusqueda.value = "";
+  inputBusqueda.value = "";
 };
 
 
@@ -96,70 +97,81 @@ function showUsers(array) {
 }
 
 
-function habilitarBTN(){
-  if (inputLastNameAgregar.value!== "" && inputLastNameAgregar.value !== "") {
+function habilitarBTN() {
+  if (inputLastNameAgregar.value !== "" && inputLastNameAgregar.value !== "") {
     btnAgregar.disabled = false;
   } else {
     btnAgregar.disabled = true;
   }
-   if (inputModificar.value !== "") {
-     btnmodificar.disabled = false;
-  
-   } else {
-     btnmodificar.disabled = true;
-   }
-   if (inputDelete.value !== "") {
-     btnDelete.disabled = false;
-   } else {
-     btnDelete.disabled = true;
-   }
+  if (inputModificar.value !== "") {
+    btnmodificar.disabled = false;
+
+  } else {
+    btnmodificar.disabled = true;
   }
-
-
-  //DELETE
-async function borrar() {
- let idBorrar = inputEliminar.value;
-
- const res = await fetch(URL + `/${idBorrar}`, {
-  method: 'DELETE',
-  headers: {
-    'Content-Type': 'application/json'
-  },
-});
-
-if (res.ok === false) {
-  console.error('Hubo un error al hacer la petición');
-  return;
+  if (inputDelete.value !== "") {
+    btnDelete.disabled = false;
+  } else {
+    btnDelete.disabled = true;
+  }
 }
 
-updateList();
- inputEliminar.value = "";
+
+//DELETE
+async function borrar() {
+  let idBorrar = inputEliminar.value;
+
+  const res = await fetch(URL + `/${idBorrar}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+  });
+
+  if (res.ok === false) {
+    console.error('Hubo un error al hacer la petición');
+    return;
+  }
+
+  updateList();
+  inputEliminar.value = "";
 }
 
 
 async function modificar() {
- let datos = {name: inputNamemodal.value, lastname: inputLastNamemodal.value};
- 
- let id = localStorage.getItem('dataModificar')
+  let datos = {
+    name: inputNamemodal.value,
+    lastname: inputLastNamemodal.value
+  };
 
- const res = await fetch(URL + `/${id}`, {
-  method: 'PUT',
-  headers: {
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify(datos)
-});
+  let id = localStorage.getItem('dataModificar')
 
-if (res.ok === false) {
-  console.error('Hubo un error al hacer la petición');
-  return;
-}
+  const res = await fetch(URL + `/${id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(datos)
+  });
 
-await updateList(); //actualiza la lista con la informacion del servidor
-inputNamemodal.value = "";
-inputLastNamemodal.value = "";
-inputModificar.value = ""; 
-btnmodificar.disabled = true;
+  if (res.ok === false) {
+    console.error('Hubo un error al hacer la petición');
+    alerta.classList.remove("hide");
+    alerta.classList.add("show");
+
+    setTimeout(function () {
+
+      alerta.classList.remove("show");
+      alerta.classList.add("hide");
+    }, "3000")
+    return;
+  }
+
+  await updateList(); //actualiza la lista con la informacion del servidor
+  inputNamemodal.value = "";
+  inputLastNamemodal.value = "";
+  inputModificar.value = "";
+  btnmodificar.disabled = true;
 
 };
 
